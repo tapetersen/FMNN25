f25be55fcb1dc3ec6e7ee405deb65a4fb004f8db
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-09-23 15:41:38 +0000

    Added skeleton for testing, function for task 4, modifications here and there. I haven't been able to get it to work though =(, the basis functions seem incorrect (see test method)
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@6 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj1/spline.py
+++ proj1/spline.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Group: Björn Lennernäs, Tobias-Alex Petersen, Johnny Sjöberg, Andy V
+# Group: Björn Lennernäs, Tobias Alex-Petersen, Johnny Sjöberg, Andy V
 #  
 
 from  __future__  import division
 from  scipy       import *
 from  matplotlib.pyplot import *
 import sys
-
+import copy 
 class Spline(object):
     """
     Sets up an equidistant knot sequence
     """
-    def __init__(self, xs, u=None):
-        self.xs = append(append(array([xs[0],xs[0]]), xs, 0),array([xs[4],xs[4]]),0)
+    def __init__(self, xs, u=None,interpol = False):
         if u is None:
-            u = arange(0, 1, 1/len(xs))
+            u = linspace(0, 1, len(xs)-2)
         self.u = r_[u[0],u[0],u,u[-1],u[-1]]
+        if(interpol):
+            xs = interpolate(xs)
+        self.xs = xs
+        
+    def interpolate(self,xs):
+        
 
     def __coeff(self,minI,maxI,u,d, j):
         try:
             alpha  = float(self.u[maxI] - u)/float(self.u[maxI]-self.u[minI])
         except ZeroDivisionError:
             return d[j+1]
+        #alpha = 1- u
+        #print "alpha = Umax - u / Umax - Umin " + " = " + \
+        #"(" + str(self.u[maxI]) + " - " + str(u) + ")/" + "(" +\
+        #str(self.u[maxI]) + " - " + str(self.u[minI]) + ") = " + str(alpha)
+        #print "d is before: " + str(d)
+        #print "j = " + str(j)
+        #print "alpha * d[j] = " + str(alpha) +"*" + str(d[j]) + " = " + str(alpha*d[j])
+        #print "(1-alpha) * d[j+1] = " + str(1-alpha) +"*" + str(d[j+1]) + " = " + str((1-alpha)*d[j+1])
         return alpha*d[j] + (1 - alpha)*d[j+1]
         
         
@@ -31,15 +44,30 @@ class Spline(object):
     """
     def __call__(self, u):
         I  = self.__hot(u)
-        d  = self.xs[I-2:I+2]
+        d  = array(self.xs[I-2:I+2])
+        print "u: " + str(u)
+        print "Index: " + str(I)
         for i in range(3):
             for j in range(0, 3-i):
-                d[j] = self.__coeff(2*i + j - 2 + I,
-                               j + 1 + i + I, u, d, j)
+                #print "\n(minI,maxI) = " + str([2*i + j - 2 + I,j + 1 + i + I])
+                k = self.__coeff(2*i + j - 2 + I,
+                               j + 1 + i + I, u, copy.copy(d), j)
+                #print "k: " + str(k)  
+                d[j] = array(k)
+                #if(d[j,0] < 0):
+                    #raise Exception
+                #print "d is after: " + str(d)
         return d[0]
         
+        
     def plot(self):
-        pass
+        over = linspace(.01,.99,200)
+        points = empty([len(over),2])
+        for i in range(len(over)):
+             points[i] = self(over[i])
+        plot(self.xs[0,:],self.xs[1,:],'*')
+        plot(points[:,0],points[:,1],'-')
+        show()
         
         
     """
@@ -51,20 +79,30 @@ class Spline(object):
         if(u < self.u[0] or u> self.u[-1]):
             return -1
         for (i, ui) in enumerate(self.u):
-            if(u < ui):
+            if(ui > u):
                 return i-1
-        
+    
+def basisFunction(knots,j):
+    d = empty([len(knots)-2])
+    d[:] = 0
+    d[j] = 1
+    return Spline(d,knots)
+    
+def test():
+    t = linspace(.001,.9,55)
+    points = empty([len(t),2])
+    for i in range(0,10):
+        b = basisFunction(linspace(0,1,100),i*10)
+        for i in range(len(t)):
+            points[i] = b(t[i])
+        plot(points[:,0],points[:,1])
+    show()
 
-"""
-Makes an instance. If running from a terminal up to three numerical
-arguments can be passes, e.g. > python spline.py 1.0,2.0,0.1, to override
-the default values set by the __init__ for the spline class.
-"""
+
 def main():
-    a = c_[arange(5), arange(5,0,-1)]
-    sp = Spline(a)
-    print sp.xs, sp.u
-    return 0
+    a = array([[0.,0.], [1.,2.], [1.5,1.5],[1.75,1.5],[2.,1.], [3.,0.]  ])
+    s = Spline(a)
+    s.plot()
 
 if __name__ == '__main__':
     main()
---
+++ proj1/test_spline.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Group: Björn Lennernäs, Tobias Alex-Petersen, Johnny Sjöberg, Andy V
+#  
+
+from  __future__  import division
+from  scipy       import *
+from  matplotlib.pyplot import *
+import spline as sp
+
+class Tester(object):
+    
+    def __init__(self):
+        pass
+        
+    def testSu(self):
+        assert('b' == 'b')
