bb3bc46a18a53b92db219d9440cb58bc9891316d
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-09-24 20:25:02 +0000

    added a bit more flesh to the interpolation method, needs to fix the call to solve banded which doesn't work now
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@10 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj1/spline.py
+++ proj1/spline.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Group: Björn Lennernäs, Tobias Alex-Petersen, Johnny Sjöberg, Andy V
+# Group: Björn Lennernäs, Tobias Alex-Petersen, Johnny Sjöberg, Andy Lundberg
 #  
 
 from  __future__  import division
 from  scipy       import *
+from  scipy import linalg as lg
 from  matplotlib.pyplot import *
 import sys
 
@@ -13,33 +14,49 @@ class Spline(object):
     Sets up an equidistant knot sequence
     """
     def __init__(self, xs, u=None,interpol = False):
+        self.equi = u is None
         if u is None:
             u = linspace(0, 1, len(xs)-2)
         self.u = r_[u[0], u[0] ,u , u[-1], u[-1]]
         if(interpol):
-            xs = interpolate(xs)
+            xs = self.interpolate(xs)
         self.xs = xs
         
+    """
+    When initilizing the spline, if interpol was set then the provided
+    points are not interpreted as control points but as points through
+    which the curve should pass. This method solves the resulting
+    equation system. 
+    """    
     def interpolate(self, xs):
-
-
-
-        pass
-        
+        # TODO: check assumption on the size of u, we need at least 3
+        # points i'd guess
+        b_x = xs[:,0]
+        b_y = xs[:,1]
+        # Check if we have an equidistant knot sequence, if yes then the
+        # matrix can be computed more efficiently
+        if(self.equi):
+            # Add efficient computation here
+        else:
+            p = (self.u + append(self.u[1:],[0.0])+ append(self.u[2:],[0.0,0.0]))/3.0
+            a = empty([len(xs),len(xs)])
+            for i in range(len(xs)):
+                for j in range(len(xs)):
+                    b = basisFunction(linspace(0, 1, len(xs)-2),j)
+                    a[i,j] = b(p[i])
+            print a
+        ab_x = dot(a,b_x)
+        ab_y = dot(a,b_y)
+        d = empty([len(xs),2])            
+        d[:,0] = lg.solve_banded([3, 4],ab_x,b_x)
+        d[:,1] = lg.solve_banded([3, 4],ab_y,b_y)
+        return d
 
     def __coeff(self, minI, maxI, u, d, j):
         try:
             alpha  = float(self.u[maxI] - u)/float(self.u[maxI]-self.u[minI])
         except ZeroDivisionError:
             return d[j+1]
-        #alpha = 1- u
-        #print "alpha = Umax - u / Umax - Umin " + " = " + \
-        #"(" + str(self.u[maxI]) + " - " + str(u) + ")/" + "(" +\
-        #str(self.u[maxI]) + " - " + str(self.u[minI]) + ") = " + str(alpha)
-        #print "d is before: " + str(d)
-        #print "j = " + str(j)
-        #print "alpha * d[j] = " + str(alpha) +"*" + str(d[j]) + " = " + str(alpha*d[j])
-        #print "(1-alpha) * d[j+1] = " + str(1-alpha) +"*" + str(d[j+1]) + " = " + str((1-alpha)*d[j+1])
         return alpha*d[j] + (1 - alpha)*d[j+1]
         
         
@@ -67,14 +84,8 @@ class Spline(object):
 
         for i in range(3):
             for j in range(0, 3-i):
-                #print "\n(minI,maxI) = " + str([2*i + j - 2 + I,j + 1 + i + I])
-                k = self.__coeff(I + i - 2 + j ,
+                d[j] = self.__coeff(I + i - 2 + j ,
                                I + j + 1 , u, d, j)
-                #print "k: " + str(k)  
-                d[j] = array(k)
-                #if(d[j,0] < 0):
-                    #raise Exception
-                #print "d is after: " + str(d)
         return d[0]
         
         
@@ -83,10 +94,11 @@ class Spline(object):
         points = self(over)
         plot(self.xs[:,0],self.xs[:,1], '*')
         plot(points[:,0],points[:,1], '+')
+        plot(points[:,0],points[:,1], linewidth=1)
         show()
         
 def basisFunction(knots,j):
-    d = empty([len(knots)-2])
+    d = empty([len(knots)+2])
     d[:] = 0
     d[j] = 1
     return Spline(d,knots)
@@ -103,8 +115,9 @@ def test():
 
 
 def main():
-    a = array([[0.,0.], [1.,2.], [1.5,1.5],[1.75,1.5],[2.,1.], [3.,0.]])
-    s = Spline(a)
+    #a = array([[0.,0.], [1.,2.], [1.5,1.5],[1.75,1.5],[2.,1.],[3.,0.]])
+    a = array(mat('0.,0.; 1.,1.3; 2.,2.9; 3.,5.; 4.,2.; 3.,0.; 2.,-1.2;0.,0.'))
+    s = Spline(a,interpol = True)
     s.plot()
 
 if __name__ == '__main__':
