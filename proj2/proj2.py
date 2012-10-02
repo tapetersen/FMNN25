b04329654ef13fe8239a9e69a84d7ceca71a7a4c
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-02 09:09:50 +0000

    skeleton, probably doesn't compile yet
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@33 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -9,6 +9,49 @@ from  scipy import linalg as lg
 from  matplotlib.pyplot import *
 import sys
 
+class FunctionTransforms(object):
+    """ A class which provides a transform of a given function. 
+    Provided transforms: 
+        - gradient
+        - hessian
+    Usage: initialize an instance of the class where the function
+    is provided and the type of transform is provided (set gradient
+    or hessian to true). The call method then returns the choosen
+    transfrom
+    The transforms are constructed by finite differences. 
+    """
+    
+    
+    def __init__(self, function, gradient = False, hessian = False):
+        if( not (gradient or hessian)):
+            raise Exception("you must specify a transform")
+        self.grad = gradient
+        self.hess = hessian
+        self.f    = function
+
+    def gradient(self,x):
+        pass
+
+    def hessian(self,x):
+        pass
+
+
+    def __call__(self, x):
+        if(self.grad):
+            return self.gradient(x)
+        if(self.hess):
+            return self.hessian(x)
+        raise Exception("Transform incompletely specified")
+
+class OptimizeFunction(object):
+    
+    def __init__(self, objective_function, function_gradient = None):
+        self.of = objective_function
+        if(function_gradient is not None):
+            self.fg = function_gradient
+        else:
+            self.fg = FunctionTransforms(objective_function,gradient=True)
+        self.hs = FunctionTransforms(objective_function,hessian=True)
 
 
 
