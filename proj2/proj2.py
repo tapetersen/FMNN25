741249ad11d9a2cbbc2fb2de11fc3ee1164f641f
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-02 08:54:29 +0000

    directory
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@32 af13a1c6-311e-4a12-858b-cde079fe1ede

---
+++ proj2/proj2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Group: Björn Lennernäs, Tobias Alex-Petersen, Johnny Sjöberg, Andy Lundberg
+#  
+
+from  __future__  import division
+from  scipy       import *
+from  scipy import linalg as lg
+from  matplotlib.pyplot import *
+import sys
+
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+    main()
