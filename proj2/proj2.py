3257b8089f337b1d0e0975204fd40e1ae3b186d4
tapetersen <tapetersen@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-03 00:13:47 +0000

    Implemented exact line search
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@45 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -6,6 +6,7 @@
 from  __future__  import division
 from  scipy       import *
 from  scipy import linalg as lg
+import scipy.optimize as opt
 from  matplotlib.pyplot import *
 import sys
 from numpy.linalg import cholesky, inv, norm, LinAlgError
@@ -35,7 +36,7 @@ class FunctionTransforms(object):
     """Approximates the gradient using (central) finite differences 
     of degree 1
     """
-    def gradient(self,x):
+    def gradient(self, x):
         grad = zeros(self.dim)
         h    = 1e-5 
         for i in range(self.dim):
@@ -48,8 +49,8 @@ class FunctionTransforms(object):
     A symmtrizing step: hessian = .5*(hessian + hessian^T) is
     also performed
     """
-    def hessian(self,x):
-        hess = zeros((self.dim,self.dim))
+    def hessian(self, x):
+        hess = zeros((self.dim, self.dim))
         h    = 1e-5
         # Approximates hessian using gradient, see 
         # http://v8doc.sas.com/sashtml/ormp/chap5/sect28.htm
@@ -66,7 +67,7 @@ class FunctionTransforms(object):
                 grad2 = (self.gradient(x+step2) - self.gradient(x-step2))/(4.*h)
                 hess[i,j] = grad1[i] + grad2[j]
         # Symmetrizing step. 
-        hess = 0.5*hess + 0.5*transpose(hess)
+        hess = 0.5*(hess + transpose(hess))
         #L = cholesky(hess) # Raises LinAlgError if (but not only if,
                            ## I guess), if hess isn't positive definite.
         return hess
@@ -98,11 +99,12 @@ class OptimizationProblem(object):
         self.hessian = FunctionTransforms(objective_function, dimension, 
                                             hessian=True)
 
+
 class OptimizationMethod(object):
     """
     Super class for various optimization methods
     """
-    def __init__(self,opt_problem):
+    def __init__(self, opt_problem):
         self.op = opt_problem
          
     def optimize(self):
@@ -111,23 +113,60 @@ class OptimizationMethod(object):
 class ClassicNewton(OptimizationMethod):
     
     
-    def __init__(self,opt_problem):
-        super(ClassicNewton,self).__init__(opt_problem)
+    def __init__(self, opt_problem):
+        super(ClassicNewton, self).__init__(opt_problem)
         
 
-    def optimize(self):
-        x = array([0.,0.0]) #starting guess
+    def optimize(self, guess=None):
+        if guess is not None:
+            x = guess
+        else:
+            x = array([0., 0.]) #starting guess
         # x* is a local minimizer if grad(f(x*)) = 0 and 
         # if its hessian is positive definite
         while(True):
             grad = self.op.gradient(x)
             if(norm(grad) < 1e-8):
                 return x
-            x = x - dot(inv(self.op.hessian(x)), self.op.gradient(x))
+            #x = x - dot(inv(self.op.hessian(x)), self.op.gradient(x))
+
+            # use cholesky decomposition as requested in task 3
+            # will throw LinAlgError if decomposition fails
+            # seems to work at least
+            factored = lg.cho_factor(self.op.hessian(x))
+            x = x - lg.cho_solve(factored, self.op.gradient(x))
+
+class NewtonExactLine(OptimizationMethod):
+    
+    
+    def __init__(self, opt_problem):
+        super(NewtonExactLine, self).__init__(opt_problem)
+        
+
+    def optimize(self, guess=None):
+        if guess is not None:
+            x = guess
+        else:
+            x = array([0., 0.]) #starting guess
+        # x* is a local minimizer if grad(f(x*)) = 0 and 
+        # if its hessian is positive definite
+        while(True):
+            grad = self.op.gradient(x)
+            if(norm(grad) < 1e-8):
+                return x
+            factored = lg.cho_factor(self.op.hessian(x))
+            direction = lg.cho_solve(factored, self.op.gradient(x))
+
+            # find step size alpha
+            # requires scipy > 0.11 and I have 0.10 // Tobias
+            #result = opt.minimize_scalar(lambda alpha: self.op.of(x-alpha*direction),
+                                 #bounds=(0, 10))
+            alpha = opt.fminbound(
+                lambda alpha: self.op.of(x - alpha*direction),
+                0, 10)
+            x = x - alpha*direction
+
             
-
- 
-
 
 def main():
     def rosenbrock(x):
@@ -139,9 +178,11 @@ def main():
         return x[0]**2 + x[0]*x[1] + x[1]**2
     def F_grad(x):
         return array([2*x[0]+x[1],x[0]+2*x[1]])
-    opt = OptimizationProblem(rosenbrock,2)
+    opt = OptimizationProblem(rosenbrock, 2)
     cn  = ClassicNewton(opt)
-    print cn.optimize()
+    print cn.optimize([-3, 7])
+    cn  = NewtonExactLine(opt)
+    print cn.optimize([-3, 7])
 
 
 if __name__ == '__main__':
