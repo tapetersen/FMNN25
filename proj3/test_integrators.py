30a98829ab6e2203f4efacf06fd6f5e2604c26f8
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-15 21:28:17 +0000

    test (nothing probably works as of now, working from windows without assimulo)
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@114 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj3/test_integrators.py
+++ proj3/test_integrators.py
@@ -8,41 +8,14 @@ import pylab as P
 from assimulo.solvers import CVode
 import nose
 from scipy import double
+import newmark as ne
 
+def test_newmark_basic_2nd_order():
+	f  = lambda x: 9.82/2.0 * t**2  + 5
+	""" Take off in a rocket at g-acc from 5m above grounds """
+	f2 = 9.82
+	nmark = Newmark(f2, y0 = 5.0, v0 = 0)
+	end = 10
+	t, y = nmark.simulate(0,end)
+    nose.tools.assert_almost_equal(y, f(end))
 
-def test_newmark():
-    pass
-
-
-def run_example(with_plots=True):
-    
-    #Define the rhs
-    def f(t,y):
-        ydot = -y[0]
-        return N.array([ydot])
-    
-    #Define an Assimulo problem
-    exp_mod = Newmark(f, y0=4.0)
-    exp_mod.name = 'Simple CVode Example'
-    
-    #Define an explicit solver
-    exp_sim = CVode(exp_mod) #Create a CVode solver
-    
-    #Sets the parameters
-    exp_sim.iter  = 'Newton' #Default 'FixedPoint'
-    exp_sim.discr = 'BDF' #Default 'Adams'
-    exp_sim.atol = [1e-4] #Default 1e-6
-    exp_sim.rtol = 1e-4 #Default 1e-6
-    
-    #Simulate
-    t1, y1 = exp_sim.simulate(5,100) #Simulate 5 seconds
-    t2, y2 = exp_sim.simulate(7) #Simulate 2 seconds more
-    
-    #Basic test
-    #nose.tools.assert_almost_equal(y2[-1], 0.00347746, 5)
-    
-    #Plot
-    if with_plots:
-        P.plot(t1, y1, color="b")
-        P.plot(t2, y2, color="r")
-        P.show()
