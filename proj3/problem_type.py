dbba0f62d26a0234efd559ec1761307afb71c509
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-15 15:42:07 +0000

    cosmetics
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@110 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj3/problem_type.py
+++ proj3/problem_type.py
@@ -15,6 +15,9 @@ class Newmark(Explicit_Problem):
     def __init__(self, rhs=None, y0=None, t0=0.0, p0=None, 
                  sw0=None):
         super(cExplicit_Problem, self).__init__(y0, t0, p0, sw0)       
+        self.h      # Stepsize 
+        self.p = zeros(1)
+        self.a = zeros(1)
         if rhs != None:
             self.rhs = rhs
     
@@ -36,8 +39,7 @@ class Newmark(Explicit_Problem):
 
 
 def run_example(with_plots=True):
-    hello = "hellooo\n"
-    print hello
+    
     #Define the rhs
     def f(t,y):
         ydot = -y[0]
@@ -46,25 +48,25 @@ def run_example(with_plots=True):
     #Define an Assimulo problem
     exp_mod = Newmark(f, y0=4.0)
     exp_mod.name = 'Simple CVode Example'
-    print hello
+    
     #Define an explicit solver
     exp_sim = CVode(exp_mod) #Create a CVode solver
-    print hello
+    
     #Sets the parameters
     exp_sim.iter  = 'Newton' #Default 'FixedPoint'
     exp_sim.discr = 'BDF' #Default 'Adams'
     exp_sim.atol = [1e-4] #Default 1e-6
     exp_sim.rtol = 1e-4 #Default 1e-6
-    print hello
+    
     #Simulate
     t1, y1 = exp_sim.simulate(5,100) #Simulate 5 seconds
     t2, y2 = exp_sim.simulate(7) #Simulate 2 seconds more
-    print hello
+    
     #Basic test
     #nose.tools.assert_almost_equal(y2[-1], 0.00347746, 5)
     
     #Plot
     if with_plots:
         P.plot(t1, y1, color="b")
-        P.plot(t2, y2, color="b")
+        P.plot(t2, y2, color="r")
         P.show()
