4d994a7c0aec8fd7012d32ef10e39cd67dd408bf
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-15 21:50:00 +0000

    minor
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@115 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj3/test_integrators.py
+++ proj3/test_integrators.py
@@ -13,8 +13,11 @@ import newmark as ne
 def test_newmark_basic_2nd_order():
 	f  = lambda x: 9.82/2.0 * t**2  + 5
 	""" Take off in a rocket at g-acc from 5m above grounds """
-	f2 = 9.82
-	nmark = Newmark(f2, y0 = 5.0, v0 = 0)
+	f2 = lambda x, y: 9.82
+	y0 = 5.0
+	v0 = 0.0
+	prob = Explicit_Problem(f2, y0)
+	nmark = Newmark(f2, v0)
 	end = 10
 	t, y = nmark.simulate(0,end)
     nose.tools.assert_almost_equal(y, f(end))
