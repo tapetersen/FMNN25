9d18aff7a77a833e715961c0d301817cd00f22c1
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-16 11:02:06 +0000

    skeleton for implicit, not working
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@117 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj3/newmark.py
+++ proj3/newmark.py
@@ -9,6 +9,7 @@ from assimulo.explicit_ode import Explicit_ODE
 import nose
 from scipy import double
 from assimulo.ode import *
+import scipy.optimize as so
 ID_COMPLETE = 3
 
 class Newmark(Explicit_ODE):
@@ -17,13 +18,13 @@ class Newmark(Explicit_ODE):
         super(Newmark, self).__init__(problem)
         self.options["h"] = 0.01
         self.f  = problem.rhs
-        self.yd1 = N.array([0.0]*len(self.y0))
         self.v = v0
-        if(beta==None or gamma==None): #No damping
-            self.explicit = True
-        else:
+        if(beta is not None and gamma is not None): 
+            self.explicit = False
             self.beta  = beta
             self.gamma = gamma
+        else:
+            self.explicit = True #No damping
     
     def _step(self, t, y, h):
         if(self.explicit):
@@ -32,15 +33,21 @@ class Newmark(Explicit_ODE):
             self.v = self.v + h/2.0 * (self.a + a)
             self.a = a
             return t+h,y
-    
+        else: # We must use solvers / implicit form
+            f = lambda p: p - (y + h*self.v + (h**2 / 2.0) * \
+                           ((1.0 - 2.*self.beta)*self.a + 2.*self.beta*self.f(t+h,p,self.v)))
+            y = so.fmin_bfgs(f,y)
+            f = lambda v: v - (self.v + h *((1.0-self.gamma)*self.a + self.gamma*(self.f(t+h,y,v))))
+            self.v = so.fmin_bfgs(f,self.v)
+            self.a = self.f(t+h,y,self.v)
+            return t+h, y
+            
     def integrate(self, t, y, tf,  opts):
         h = self.options["h"]
         h = min(h, abs(tf-t))
         tr = []
         yr = []
         self.a = self.f(t,y,self.v)
-        print self.a
-        
         while t+h < tf:
             t, y = self._step(t,y,h)
             tr.append(t)
--- proj3/test_integrators.py
+++ proj3/test_integrators.py
@@ -6,7 +6,7 @@ from assimulo.exception import *
 import nose
 from newmark import Newmark
 
-def test_newmark_basic_2nd_order():
+def test_newmark_basic_2nd_order_no_damping():
     f  = lambda t: 9.82/2.0 * t**2  + 5
     """ Take off in a rocket at g-acc from 5m above grounds """
     f2 = lambda t, x, y: 9.82
@@ -19,3 +19,15 @@ def test_newmark_basic_2nd_order():
     print y[-1], f(end), t[-1]
     nose.tools.assert_almost_equal(y[-1], f(end))
 
+def test_newmark_basic_2nd_order_damping():
+    f  = lambda t: 9.82/2.0 * t**2  + 5
+    """ Take off in a rocket at g-acc from 5m above grounds """
+    f2 = lambda t, x, y: 9.82
+    y0 = 5.0
+    v0 = 0.0
+    prob = Explicit_Problem(f2, y0)
+    nmark = Newmark(prob, v0, beta=0.3, gamma = 0.3)
+    end = 10.0
+    t, y = nmark.simulate(end)
+    print y[-1], f(end), t[-1]
+    nose.tools.assert_almost_equal(y[-1], f(end))
