0d6356e92e5b567d0855524e8346aae029d2d323
BjornL <BjornL@af13a1c6-311e-4a12-858b-cde079fe1ede>
2012-10-23 11:37:02 +0000

    extended basic test to test different values of gamma and beta
    git-svn-id: https://xp-dev.com/svn/FMNN25_Projects/trunk@124 af13a1c6-311e-4a12-858b-cde079fe1ede

--- proj3/test_integrators.py
+++ proj3/test_integrators.py
@@ -5,6 +5,7 @@ from assimulo.problem import Explicit_Problem, cExplicit_Problem
 from assimulo.exception import *
 import nose
 from newmark import Newmark
+from scipy import array
 
 def test_newmark_basic_2nd_order_no_damping():
     f  = lambda t: 9.82/2.0 * t**2  + 5
@@ -26,8 +27,10 @@ def test_newmark_basic_2nd_order_damping():
     y0 = 5.0
     v0 = 10.0
     prob = Explicit_Problem(f2, y0)
-    nmark = Newmark(prob, v0, beta=0.3, gamma = 0.3)
-    end = 10.0
-    t, y = nmark.simulate(end)
-    print y[-1], f(end), t[-1]
-    nose.tools.assert_almost_equal(y[-1], f(end))
+    vals = array([0.0,0.25,0.5,0.75,1.0])
+    for beta in vals:
+        for gamma in vals:                
+            nmark = Newmark(prob, v0, beta=beta, gamma=gamma)
+            end = 10.0
+            t, y = nmark.simulate(end)
+            nose.tools.assert_almost_equal(y[-1], f(end))
